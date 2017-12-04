78ab174e016cbf9d3fc0c7ae6ac7fa443e160f00
zxcv <zxcv@zxcv.zxcv>
2017-12-04 07:06:33 -0800

    Added DDS_Loader: It's broken however. :(

---
+++ src/DDS_Loader.cpp
@@ -0,0 +1,8 @@
+//
+//  DDS_Loader.cpp
+//  final471project
+//
+//  Created by Alex Saalberg on 12/3/17.
+//
+
+#include "DDS_Loader.hpp"
---
+++ src/DDS_Loader.hpp
@@ -0,0 +1,13 @@
+//
+//  DDS_Loader.hpp
+//  final471project
+//
+//  Created by Alex Saalberg on 12/3/17.
+//
+
+#ifndef DDS_Loader_hpp
+#define DDS_Loader_hpp
+
+#include <stdio.h>
+
+#endif /* DDS_Loader_hpp */
