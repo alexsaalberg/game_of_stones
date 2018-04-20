93608fb8f5d02db792dde79c9cfaab16d15efebc
Alex Saalberg <asaalber@calpoly.edu>
2018-04-19 21:21:27 -0700

    Removed unused voxel functions from RenderSystem.

--- src/RenderSystem.hpp
+++ src/RenderSystem.hpp
@@ -32,20 +32,9 @@ class RenderSystem : System {
 public:
     std::shared_ptr<EntityManager> entity_manager;
     WindowManager* window_manager;
-    //std::vector<VoxelMeshData> voxel_meshes;
-    std::multimap<Entity_Id, VoxelMeshData> voxel_meshes;
     
     void draw(double t, std::shared_ptr<Program> program);
     void draw_entities(double t, std::shared_ptr<Program> program);
-    void draw_voxels(double t, std::shared_ptr<Program> program);
-    
-    //Voxel helpers
-    void calculate_mesh(double t, Entity_Id id, Voxel_Component *voxel_component);
-    
-    template <typename MeshType>
-    void addMesh(double t, Entity_Id id, const MeshType& surfaceMesh, const PolyVox::Vector3DInt32& translation = PolyVox::Vector3DInt32(0, 0, 0), float scale = 1.0f);
-    
-    void addMeshData(Entity_Id id, VoxelMeshData meshData);
     
     //Camera Functions
     void setMVPE(double t, std::shared_ptr<Program> program);
