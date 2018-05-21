46426fa8ed9189d42217a872079d473c22b49b8d
Alex Saalberg <asaalber@calpoly.edu>
2018-05-21 14:08:34 -0700

    Added PlayerSystem

---
+++ src/PlayerSystem.cpp
@@ -0,0 +1,8 @@
+//
+//  PlayerSystem.cpp
+//  CastleSim
+//
+//  Created by Alex Saalberg on 5/20/18.
+//
+
+#include "PlayerSystem.hpp"
---
+++ src/PlayerSystem.hpp
@@ -0,0 +1,13 @@
+//
+//  PlayerSystem.hpp
+//  CastleSim
+//
+//  Created by Alex Saalberg on 5/20/18.
+//
+
+#ifndef PlayerSystem_hpp
+#define PlayerSystem_hpp
+
+#include <stdio.h>
+
+#endif /* PlayerSystem_hpp */
